c2c967b9b3bcf0e7047216286598e66d4910913e
mostafahashemi21-hash <mostafa_hashemi21@yahoo.com>
2026-06-20 22:10:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print("Bot Started")
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "سلام 👋\nبه ربات SAM خوش آمدی."
+    )
+
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "/start\n/help\n/newtask"
+    )
+
+async def newtask(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = " ".join(context.args)
+
+    if not text:
+        await update.message.reply_text(
+            "مثال:\n/newtask خرید خانه"
+        )
+        return
+
+    await update.message.reply_text(
+        f"✅ کار ثبت شد:\n{text}"
+    )
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("help", help_command))
+app.add_handler(CommandHandler("newtask", newtask))
+
+app.run_polling()
