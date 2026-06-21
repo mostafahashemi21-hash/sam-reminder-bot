3425c9f8a36794cbf2f14b6a6e29a5769846fc64
mostafahashemi21-hash <mostafa_hashemi21@yahoo.com>
2026-06-21 21:33:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -848,10 +848,10 @@ if __name__ == "__main__":
 
     job_queue = app.job_queue
 
-    job_queue.run_repeating(
-        check_tasks,
-        interval=3600,
-        first=10
-    )
-    
-    app.run_polling(drop_pending_updates=True)
+job_queue.run_repeating(
+    check_tasks,
+    interval=60,
+    first=10
+)
+
+app.run_polling(drop_pending_updates=True)
