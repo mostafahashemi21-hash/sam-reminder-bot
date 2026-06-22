0a0e0f868b1503b4d6c5491861178a7eac4073bb
mostafahashemi21-hash <mostafa_hashemi21@yahoo.com>
2026-06-22 05:19:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -718,10 +718,10 @@ async def buttons(
 
     text = update.message.text
 
-    elif text == "⏱ پیگیری":
+     elif text == "⏱ پیگیری":
 
-    await open_tasks_panel(update, context)
-    return
+        await open_tasks_panel(update, context)
+        return
 
     elif text == "🧠 تحلیل چت":
 
